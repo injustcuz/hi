b446e92117472bb88949333957ecafb54fd2d6b3
Danny <danobird1@gmail.com>
2025-05-07 15:40:37 -0700

    route

--- app/api/channel-casts/route.js
+++ app/api/channel-casts/route.js
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 import axios from 'axios';
 
 export async function GET() {
-    const CHANNEL_ID = "300";
-    const FARCASTER_API_URL = `https://api.neynar.com/v2/farcaster/channel/casts?id=${300}&limit=20`;
+    const CHANNEL_ID = "sound4sound";
+    const FARCASTER_API_URL = `https://api.neynar.com/v2/farcaster/channel/casts?id=${sound4sound}&limit=20`;
     
     const response = await axios.get(FARCASTER_API_URL, {
         headers: {
